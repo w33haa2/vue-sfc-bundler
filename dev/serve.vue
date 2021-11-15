82618ebf29459dd9f0567227323a3694186b95b2
JAN REY SUAREZ <w33haa2@gmail.com>
2021-11-15 21:45:05 +0800

    feat: initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import TestComponent from '@/test-component.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    TestComponent
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <test-component />
+  </div>
+</template>
